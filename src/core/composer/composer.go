2805961b1c2f2fd981007512e6f8b92800a91b97
Shuvojit Sarkar <shuvojit@healthi.in>
2023-02-26 16:06:01 +0530

    simple test added

---
+++ src/core/composer/composer.go
@@ -0,0 +1,10 @@
+package composer
+
+import (
+	"github.com/anasmohammad611/compose-cli/core/errors"
+	"github.com/anasmohammad611/compose-cli/core/types"
+)
+
+func AddConfig(dockerComposeStr string, newItem types.Template) (string, error) {
+	return "", errors.ErrInvalidYaml
+}
